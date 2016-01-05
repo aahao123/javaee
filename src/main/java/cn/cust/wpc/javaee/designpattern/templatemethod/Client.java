f6c7df6bbf224607508df8dbc62c175460672f10
pengcheng.wang <1195746619@qq.com>
2016-01-05 22:39:11 +0800

    模板方法模式通用代码清单

---
+++ src/main/java/cn/cust/wpc/javaee/designpattern/templatemethod/Client.java
@@ -0,0 +1,16 @@
+package cn.cust.wpc.javaee.designpattern.templatemethod;
+
+/**
+ * @author: pengcheng.wang
+ * @since: 16/1/5 下午10:24
+ */
+public class Client {
+    public static void main(String[] args) {
+        AbstractClass class1 = new ConcreteClass1();
+        AbstractClass class2 = new ConcreteClass2();
+
+        //调用模板方法
+        class1.templateMethod();
+        class2.templateMethod();
+    }
+}
---
+++ src/main/java/cn/cust/wpc/javaee/designpattern/templatemethod/ConcreteClass2.java
@@ -0,0 +1,18 @@
+package cn.cust.wpc.javaee.designpattern.templatemethod;
+
+/**
+ * @author: pengcheng.wang
+ * @since: 16/1/5 下午10:20
+ */
+public class ConcreteClass2 extends AbstractClass {
+    //实现基本方法
+    @Override
+    protected void doSomething() {
+        //业务逻辑处理
+    }
+
+    @Override
+    protected void doAnything() {
+        //业务逻辑处理
+    }
+}
